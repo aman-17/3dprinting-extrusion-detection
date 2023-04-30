7f3db62c0fbf60be45c50a7b811f8a3a27fe075a
Aman Rangapur <amanrangapur@gmail.com>
2023-04-30 12:23:16 -0500

    updated model

--- multihead2.py
+++ multihead2.py
@@ -1,10 +1,11 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
+import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, models
-from PIL import Image
+import torch.nn as nn
 import pandas as pd
+import numpy as np
+from PIL import Image
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
@@ -21,7 +22,7 @@ class PrintingDefectsDataset(Dataset):
     def __getitem__(self, idx):
         img_name = self.root_dir + self.printing_defects.iloc[idx, 0]
         image = Image.open(img_name)
-        label = self.printing_defects.iloc[idx, 3]  # change this line
+        label = self.printing_defects.iloc[idx, 3] 
         
         if self.transform:
             image = self.transform(image)
@@ -44,78 +45,50 @@ val_size = len(dataset) - train_size
 train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
 
 batch_size = 64
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
-
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
 print(len(train_loader))
 
+model = models.resnet18(pretrained=True)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 1)
+model = model.to(device)
 
-class MultiHeadResidualAttentionNet(nn.Module):
-    def __init__(self, num_classes=2):
-        super(MultiHeadResidualAttentionNet, self).__init__()
-        self.resnet = models.resnet50(pretrained=True)
-        self.resnet.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
-        self.resnet.fc = nn.Identity()
-
-        self.head1 = nn.Sequential(
-            nn.Linear(in_features=2048, out_features=512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.2),
-            nn.Linear(in_features=512, out_features=num_classes)
-        )
-
-        self.head2 = nn.Sequential(
-            nn.Linear(in_features=2048, out_features=512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.2),
-            nn.Linear(in_features=512, out_features=num_classes)
-        )
-
-    def forward(self, x):
-        x = self.resnet(x)
-        x1 = self.head1(x)
-        x2 = self.head2(x)
-        return x1, x2
-
-model = MultiHeadResidualAttentionNet()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters())
+criterion = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 num_epochs = 10
 for epoch in range(num_epochs):
-    train_loss = 0.0
-    for i, (images, labels) in enumerate(train_loader):
+    model.train()
+    train_loss = 0
+    train_acc = 0
+    for images, labels in train_loader:
         images = images.to(device)
-        labels = labels.to(device)
-
+        labels = labels.float().to(device)
         optimizer.zero_grad()
-
         outputs = model(images)
-        loss1 = criterion(outputs[0], labels)
-        loss2 = criterion(outputs[1], labels)
-        loss = loss1 + loss2
+        loss = criterion(outputs.squeeze(), labels)
         loss.backward()
         optimizer.step()
-
-        train_loss += loss.item() * images.size(0)
-        if (i + 1) % 10 == 0:
-            print(f"Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}")
+        train_loss += loss.item()
+        preds = torch.round(torch.sigmoid(outputs))
+        train_acc += torch.sum(preds == labels.data)
+    train_loss /= len(train_loader.dataset)
+    train_acc = float(train_acc) / len(train_loader.dataset)
     
     model.eval()
-    val_loss = 0.0
+    val_loss = 0
+    val_acc = 0
     with torch.no_grad():
         for images, labels in val_loader:
             images = images.to(device)
-            labels = labels.to(device)
+            labels = labels.float().to(device)
             outputs = model(images)
-            loss1 = criterion(outputs[0], labels)
-            loss2 = criterion(outputs[1], labels)
-            loss = loss1 + loss2
-
-            val_loss += loss.item() * images.size(0)
-    
-    train_loss /= len(train_loader.dataset)
+            loss = criterion(outputs.squeeze(), labels)
+            val_loss += loss.item()
+            preds = torch.round(torch.sigmoid(outputs))
+            val_acc += torch.sum(preds == labels.data)
     val_loss /= len(val_loader.dataset)
-    
-    print(f"Epoch [{epoch + 1}/{num_epochs}], Training Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}")
-
+    val_acc = float(val_acc) / len(val_loader.dataset)
+    print('Epoch [{}/{}], Train Loss: {:.4f}, Train Acc: {:.4f}, Val Loss: {:.4f}, Val Acc: {:.4f}'
+          .format(epoch+1, num_epochs, train_loss, train_acc, val_loss, val_acc))
